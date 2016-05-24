c31e47367d105355d960eb5fe317398f1eb9f9f3
Denis Kubashevskiy <dehax12@gmail.com>
2016-05-25 00:17:04 +0300

    Создано решение с проектом приложения.

---
+++ DWin3D/WinMain.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	MessageBoxW(NULL, L"Hello World!", L"DWin3D", MB_OK);
+
+	return 0;
+}
